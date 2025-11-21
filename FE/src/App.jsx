2348eb66d4df7d34dc339c71efe139fb490b11aa
Niel-D22 <danielwarouw01@gmail.com>
2025-11-21 10:45:35 +0800

    Setup FE BE

---
+++ FE/src/App.jsx
@@ -0,0 +1,18 @@
+
+import "./App.css";
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <h1 class="text-3xl bg-amber-400 font-bold underline">test mine
+          
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
